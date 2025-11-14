6cb5389dfc76e4b28b1f7f0a51837e047e161c75
Mowmita Yasmin <meite@academin.rrc.ca>
2025-11-14 09:27:35 -0600

    error handling, response models, server & config updates

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import setupSwagger from "./config/swagger";
+import corsOptions from "./config/cors";
+import { useExpressServer } from "routing-controllers";
+import { FaqController } from "./api/v1/controllers/faqController";
 
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+
+// setup the controllers and use the /api/v1 prefix for the routes
+useExpressServer(app, {
+  routePrefix: "/api/v1",
+  controllers: [FaqController],
+  cors: corsOptions,
+  defaultErrorHandler: false,
+});
+
+dotenv.config();
+
+setupSwagger(app);
+
+app.get("/", (_req, res) => {
+  res.send("Got response from backend!");
+});
+
+export default app;
