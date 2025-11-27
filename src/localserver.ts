390edb7e02bfe48076d90031b6e6f3a970b033ec
Tong Xu <txu2@rrc.ca>
2025-11-27 04:21:12 -0600

    Update vercel server in index.ts

---
+++ src/localserver.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+
+import { Server } from "http";
+
+const PORT: string | 4000 = process.env.PORT || 4000;
+
+const server: Server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default server;
